7cc09a43e0e453f27f366848b3de7f298f89a2fd
BieNew22 <newbie11004@gmail.com>
2025-09-12 21:51:06 +0900

    Fix: title wrapping into multiple lines on mobile view.

--- src/app/_component/Header.tsx
+++ src/app/_component/Header.tsx
@@ -4,12 +4,14 @@ import ThemeToggle from "./ThemeToggle";
 export default function Header() {
   return (
     <header className="border-b">
-      <div className="mx-auto max-w-5xl px-4 py-3 flex items-center justify-center">
-        <Link href="/" className="text-3xl font-sourcecodepro font-bold">
+      <div className="relative mx-auto max-w-5xl px-4 py-3 grid grid-cols-3 items-center">
+        <div />
+        <Link href="/" className="justify-self-center whitespace-nowrap sm:text-3xl text-2xl font-sourcecodepro font-bold">
           bienew's blog
         </Link>
-
-        <ThemeToggle />
+        <div className="justify-self-end" >
+          <ThemeToggle />
+        </div>
       </div>
     </header>
   );
