1e845515a42f8a97c4feed11722cfe567820a1b7
BieNew22 <newbie11004@gmail.com>
2025-09-13 17:50:31 +0900

    Design: change header shadow

--- src/app/_component/Header.tsx
+++ src/app/_component/Header.tsx
@@ -3,7 +3,7 @@ import ThemeToggle from "./ThemeToggle";
 
 export default function Header() {
   return (
-    <header className="border-b">
+    <header className="shadow-sm">
       <div className="relative mx-auto max-w-5xl px-4 py-3 grid grid-cols-3 items-center">
         <div />
         <Link href="/" className="justify-self-center whitespace-nowrap sm:text-3xl text-2xl font-sourcecodepro font-bold">
