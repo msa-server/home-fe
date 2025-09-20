6348c7546a4110fb874c09911124a3d81103c01f
BieNew22 <newbie11004@gmail.com>
2025-09-20 11:15:46 +0900

    Feat: complete posts page.

--- src/app/(shell)/posts/page.tsx
+++ src/app/(shell)/posts/page.tsx
@@ -1,9 +1,9 @@
-// app/posts/page.tsx
-import Sidebar from "../../_temp/sidebar";
-import { Suspense } from "react";
-import Link from "next/link";
 import PageNavigation from "@/app/_component/PageNavigation";
 import PostCard from "@/app/_component/PostCard";
+
+type ArticleCount = {
+  articleCount: number;
+}
 
 type ArticleTag = {
   tagId: number;
@@ -11,41 +11,52 @@ type ArticleTag = {
   count: number;
 };
 
+type Series = {
+  seriesId: number;
+  seriesName: string;
+  articleCount: number;
+}
+
 type Article = {
   articleId: number;
   title: string;
   content: string;
   articleTags: ArticleTag[];
+  series: Series;
   createdAt: string;
   modifiedAt: string;
-};
-
-type ListResponse = {
-  articles: Article[];
-  articleCount: number;
 };
 
 async function fetchArticles(
   page: number,
   size: number
-): Promise<ListResponse> {
-  const url = `http:/localhost:9000/v1/articles?tagId=1&page=${page}&pageSize=${size}`;
+): Promise<Article[]> {
+  const url = `http:/localhost:9000/v1/articles?page=${page}&pageSize=${size}`;
   const res = await fetch(url, { cache: "no-store" });
   if (!res.ok) throw new Error("게시글을 불러오지 못했습니다.");
+
+  const data: Article[] = await res.json();
+  return data;
+}
+
+async function fetchTotalArticles(): Promise<ArticleCount> {
+  const url = `http:/localhost:9000/v1/articles/count`;
+  const res = await fetch(url);
+  if (!res.ok) throw new Error("게시글 수 불러오지 못했습니다.");
+
   return res.json();
 }
 
-export default async function PostsPage({
-  searchParams,
-}: {
-  searchParams?: { page?: string };
-}) {
-  const page = Math.max(parseInt(searchParams?.page ?? "1", 10) || 1, 1);
+export default async function PostsPage({ searchParams }: { searchParams: Promise<{ page?: string }> }) {
+  const { articleCount } = await fetchTotalArticles();
+  const { page } = await searchParams;
   const size = 12;
 
-  const { articles, articleCount } = await fetchArticles(page, size);
-
   const totalPages = Math.max(Math.ceil(articleCount / size), 1);
+
+  const currentPage = Math.min(Math.max(parseInt(page ?? "1", 10) || 1, 1), totalPages);
+  
+  const articles = await fetchArticles(currentPage, size);
 
   return (
     <main className="mx-auto max-w-4xl flex flex-col gap-8">
@@ -57,7 +68,7 @@ export default async function PostsPage({
             href={`/post/${post.articleId}`}
             title={`${post.title}`}
             coverUrl={`/profile.webp`}    // TODO : 실제 썸네일 URL로 교체
-            category="SERIES"             // TODO : 실제 게시글 시리즈 교체
+            category={`${post.series.seriesName}`}
             authorName="bienew22"
             authorAvatarUrl="/profile.webp"
             createdAt={post.createdAt.slice(0, 10).replaceAll("-", ".")} />
@@ -65,7 +76,7 @@ export default async function PostsPage({
       </section>
 
       <PageNavigation
-          page={page}
+          page={currentPage}
           totalPages={totalPages}
           size={size}
           basePath="/posts" />
