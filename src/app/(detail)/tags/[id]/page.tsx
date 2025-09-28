175392b3cf921580db4c288f2ced834de9251fc5
BieNew22 <newbie11004@gmail.com>
2025-09-28 17:41:24 +0900

    Feat: implement tag detail page structure.

--- src/app/(detail)/tags/[id]/page.tsx
+++ src/app/(detail)/tags/[id]/page.tsx
@@ -1,28 +1,74 @@
 import { TagDetail } from "@/types/tags";
-// import styles from "@/styles/Divider.module.css"
+import PageNavigation from "@/component/PageNavigation";
+import { Article } from "@/types/article";
 
 async function fetchTag(id: string): Promise<TagDetail> {
-    const res = await fetch("http://localhost:9000/v1/tags/" + id, { cache: "no-store", method: "GET" });
-    if (!res.ok) throw new Error("시리즈 정보 불러오지 못했습니다.");
-    const all: TagDetail = await res.json();
-    
-    return all;
+  const res = await fetch(`http://localhost:9000/v1/tags/${id}`, {
+    cache: "no-store",
+  });
+  if (!res.ok) throw new Error("시리즈 정보 불러오지 못했습니다.");
+  return res.json();
+}
+
+async function fetchTagArticles(
+  tagId: string,
+  page: number,
+  size: number
+): Promise<Article[]> {
+  const url = `http:/localhost:9000/v1/tags/${tagId}/articles?page=${page}&pageSize=${size}`;
+  const res = await fetch(url, { cache: "no-store" });
+  if (!res.ok) throw new Error("게시글을 불러오지 못했습니다.");
+
+  const data: Article[] = await res.json();
+  return data;
 }
 
 export default async function TagsDetailView({
-  params,
-}: { params: { id: string } }) {
+  params, searchParams
+}: {
+  params: { id: string };
+  searchParams: { page?: string };
+}) {
+  const { id } = await params;
+  const { page } = await searchParams;
+  const size = 8;
 
-  const { id } = await params; 
-  const tagInfo = await fetchTag(id);
+  const tag: TagDetail = await fetchTag(id);
+
+  const totalPages = Math.max(Math.ceil(tag.count / size), 1);
+
+  const currentPage = Math.min(Math.max(parseInt(page ?? "1", 10) || 1, 1), totalPages);
+  
+  const articles = await fetchTagArticles(tag.tagId, currentPage, size);
+
+  console.log("totalPages = " + totalPages);
 
   return (
-    <main className="mx-auto max-w-5xl px-4 py-16">
-      <section className="text-center select-none">
-        <h1 className="text-5xl font-bold tracking-tight">{tagInfo.tagName}</h1>
-        <div className=".divider" aria-hidden="true" />
-        <p className="mt-3 text-gray-600">공부한 내용을 정리합니다</p>
+    <main className="mx-auto max-w-4xl px-4 py-3 font-noto">
+      <section className="select-none">
+        {/* SERIES 배지 */}
+        <span className="inline-block bg-black dark:bg-white text-white dark:text-black text-sm font-extrabold tracking-wider uppercase px-3 py-1 rounded">
+          Tags
+        </span>
+
+        {/* 타이틀 */}
+        <h1 className="mt-4 text-4xl md:text-5xl font-extrabold">
+          {tag.tagName}
+        </h1>
+
+        {/* 얇은 구분선 */}
+        <div className="mt-8 border-t border-gray-200 dark:border-gray-700" />
       </section>
+
+      <section>
+        {/* TODO: 실제 게시글 전시하기. */}
+        게시글 섹션</section>
+
+       <PageNavigation
+                page={currentPage}
+                totalPages={totalPages}
+                size={size}
+                basePath={"/tags/" + tag.tagId} />
     </main>
   );
 }
